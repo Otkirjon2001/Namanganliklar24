b30e51b35074d222d36496bb9a24c976d48d03d8
Otkirjon2001 <otkirsalimboyev@gmail.com>
2022-05-31 22:49:23 +0300

    Add contact page BEM technology

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-// const elSiteModal = document.querySelector(".js-modal");
-// const elCloseButton = elSiteModal.querySelector(".js-close-box");
-// if(elSiteModal){
-//   elCloseButton.addEventListener("click", function () {
-//     elSiteModal.classList.add("none");
-//   });
-// }
-// setTimeout(function(){
-//   elSiteModal.classList.add("modal--open");
-// }, 3000)
+const elSiteModal = document.querySelector(".js-modal");
+const elCloseButton = elSiteModal.querySelector(".js-close-box");
+if(elSiteModal){
+  elCloseButton.addEventListener("click", function () {
+    elSiteModal.classList.add("none");
+  });
+}
+setTimeout(function(){
+  elSiteModal.classList.add("modal--open");
+}, 3000)
